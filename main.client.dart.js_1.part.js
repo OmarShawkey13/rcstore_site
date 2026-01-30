54e5ab9155c25ea4b93d525ef9f9221901fdb63c
OmarShawkey13 <superbad13245@gmail.com>
2026-01-30 17:01:31 +0200

    change frame work

---
+++ main.client.dart.js_1.part.js
@@ -0,0 +1,152 @@
+((a,b)=>{a[b]=a[b]||{}})(self,"$__dart_deferred_initializers__")
+$__dart_deferred_initializers__.current=function(a,b,c,$){var C,D,A={dV:function dV(d){this.a=d},dY:function dY(d){this.a=d},aV:function aV(d,e,f){this.d=d
+this.w=e
+this.a=f},dX:function dX(d){this.a=d},dT:function dT(d,e,f,g){var _=this
+_.w=d
+_.z=e
+_.Q=f
+_.a=g},dW:function dW(d,e,f){this.w=d
+this.Q=e
+this.a=f},dS:function dS(d,e,f,g,h,i){var _=this
+_.d=d
+_.e=e
+_.y=f
+_.z=g
+_.at=h
+_.a=i},eD:function eD(d,e){this.a=d
+this.b=e},aY:function aY(d,e,f){this.d=d
+this.w=e
+this.a=f},dI:function dI(d,e){this.a=d
+this.b=e},G:function G(){},dd:function dd(d,e,f){var _=this
+_.c=_.b=_.a=_.cy=_.ry=null
+_.d=d
+_.e=null
+_.f=e
+_.w=_.r=null
+_.x=f
+_.Q=_.z=_.y=null
+_.as=!1
+_.at=!0
+_.ax=!1
+_.CW=null
+_.cx=!1},bA:function bA(d,e,f,g,h,i){var _=this
+_.c=d
+_.d=e
+_.e=f
+_.f=g
+_.r=h
+_.a=i},cO:function cO(d){this.a=d},el:function el(){},dm:function dm(d){this.a=d},
+jr(){return new A.aG(null)},
+aG:function aG(d){this.a=d},
+fA(d,e,f,g){return C.Z(y.w,y.a)},
+i9(d,e){var x=null
+return new C.cl(d,x,x,x,x,e)}},B
+C=c[0]
+D=c[2]
+A=a.updateHolder(c[3],A)
+B=c[4]
+A.dV.prototype={
+F(d){var x=null
+return new C.J("h1",x,"hero-title",x,x,x,B.Q,x)}}
+A.dY.prototype={
+F(d){var x=null
+return new C.J("section",x,"hero",x,x,x,B.P,x)}}
+A.aV.prototype={
+F(d){var x=null
+return new C.J("div",x,this.d,x,x,x,this.w,x)}}
+A.dX.prototype={
+F(d){var x=null
+return new C.J("p",x,"hero-desc",x,x,x,B.S,x)}}
+A.dT.prototype={
+F(d){var x=null,w=y.w,v=C.Z(w,y.a)
+v.E(0,this.z)
+v.E(0,A.iF().$1$1$onClick(x,y.v))
+return new C.J("button",x,this.w,x,C.Z(w,w),v,this.Q,x)}}
+A.dW.prototype={
+F(d){var x=null,w=y.w
+w=C.Z(w,w)
+w.q(0,"src",this.w)
+return new C.J("img",x,x,this.Q,w,x,x,x)}}
+A.dS.prototype={
+F(d){var x=this,w=y.w,v=C.Z(w,w)
+v.q(0,"href",x.d)
+v.q(0,"target","_blank")
+w=C.Z(w,y.a)
+w.E(0,A.iF().$1$1$onClick(null,y.v))
+return new C.J("a",null,x.y,x.z,v,w,x.at,null)}}
+A.eD.prototype={
+aJ(){return"Target."+this.b}}
+A.aY.prototype={
+F(d){var x=null
+return new C.J("span",x,this.d,x,x,x,this.w,x)}}
+A.dI.prototype={}
+A.G.prototype={
+P(){var x=($.K+1)%16777215
+$.K=x
+return new A.dd(x,this,D.c)}}
+A.dd.prototype={
+gk(){return y.i.a(C.d.prototype.gk.call(this))},
+C(){if(this.w.c)this.r.toString
+this.b1()},
+a9(d){y.i.a(C.d.prototype.gk.call(this))
+return!0},
+bn(){return y.i.a(C.d.prototype.gk.call(this)).F(this)},
+a3(){this.w.toString
+this.bI()}}
+A.bA.prototype={
+F(d){var x=this,w=x.c,v=x.r?"mockup front floating":"mockup floating",u=y.w
+return new A.dS(w,B.Z,v,A.i9(null,C.cW(["transform","rotate("+C.n(x.e)+"rad)","animation-delay",C.n(x.f)+"s","cursor","zoom-in"],u,u)),C.i([new A.dW(w,A.i9(new A.dI("px",x.d),C.cW(["border-radius","30px","pointer-events","none"],u,u)),null)],y.F),null)}}
+A.cO.prototype={
+F(d){return new A.aV("hero-copy",C.i([B.aq,B.ar,new A.dT("cta-btn",C.cW(["click",new A.el()],y.w,y.a),B.N,null)],y.F),null)}}
+A.dm.prototype={
+F(d){return B.ap}}
+A.aG.prototype={
+F(d){return B.as}}
+var z=a.updateTypes(["z<b,~(m)>({onChange:~(0^)?,onClick:~()?,onInput:~(0^)?})<k?>"])
+A.el.prototype={
+$1(d){var x
+C.x(d).preventDefault()
+x=C.B(C.x(b.G.document).querySelector(".apps-section"))
+if(x!=null)x.scrollIntoView()},
+$S:4};(function installTearOffs(){var x=a.installStaticTearOff
+x(A,"iF",0,null,["$1$3$onChange$onClick$onInput","$0","$1$0","$1$1$onClick"],["fA",function(){return A.fA(null,null,null,y.b)},function(d){return A.fA(null,null,null,d)},function(d,e){return A.fA(null,d,null,e)}],0,0)})();(function inheritance(){var x=a.inherit,w=a.inheritMany
+x(A.G,C.l)
+w(A.G,[A.dV,A.dY,A.aV,A.dX,A.dT,A.dW,A.dS,A.aY,A.bA,A.cO,A.dm,A.aG])
+x(A.eD,C.dB)
+x(A.dI,C.aS)
+x(A.dd,C.b_)
+x(A.el,C.ae)})()
+C.ig(b.typeUniverse,JSON.parse('{"dV":{"G":[],"l":[]},"dY":{"G":[],"l":[]},"aV":{"G":[],"l":[]},"dX":{"G":[],"l":[]},"dT":{"G":[],"l":[]},"dW":{"G":[],"l":[]},"dS":{"G":[],"l":[]},"aY":{"G":[],"l":[]},"dI":{"eG":[]},"G":{"l":[]},"dd":{"d":[],"O":[]},"bA":{"G":[],"l":[]},"cO":{"G":[],"l":[]},"dm":{"G":[],"l":[]},"aG":{"G":[],"l":[]}}'))
+var y={F:C.a9("w<l>"),i:C.a9("G"),w:C.a9("b"),b:C.a9("@"),v:C.a9("~"),a:C.a9("~(m)")};(function constants(){var x=a.makeConstList
+B.a2=new C.ah("Explore Apps",null)
+B.O=x([B.a2],y.F)
+B.av=new A.aY(null,B.O,null)
+B.a0=new C.ah(" \u2193",null)
+B.R=x([B.a0],y.F)
+B.at=new A.aY(null,B.R,null)
+B.I=x([B.at],y.F)
+B.aw=new A.aY("arrow",B.I,null)
+B.N=x([B.av,B.aw],y.F)
+B.D=new A.cO(null)
+B.ag=new A.dm(null)
+B.L=x([B.D,B.ag],y.F)
+B.an=new A.aV("hero-grid",B.L,null)
+B.J=x([B.an],y.F)
+B.ao=new A.aV("hero-container",B.J,null)
+B.P=x([B.ao],y.F)
+B.a_=new C.ah("RCMods Store",null)
+B.K=x([B.a_],y.F)
+B.au=new A.aY(null,B.K,null)
+B.Q=x([B.au],y.F)
+B.a1=new C.ah("Stand out from the crowd and let your phone speak with elegance. You are unique for being part of the RC family, and we are here for you.",null)
+B.S=x([B.a1],y.F)
+B.Z=new A.eD(1,"blank")
+B.B=new A.bA("images/mockup_whatsapp.webp",420,-0.15,0,!1,null)
+B.C=new A.bA("images/mockup_telegram.webp",450,0.1,2.5,!0,null)
+B.M=x([B.B,B.C],y.F)
+B.ap=new A.aV("hero-visual",B.M,null)
+B.aq=new A.dV(null)
+B.ar=new A.dX(null)
+B.as=new A.dY(null)})()};
+(a=>{a["K/rZ9swbqCNElshwIOxuulrN3hM="]=a.current})($__dart_deferred_initializers__);
+//# sourceMappingURL=main.client.dart.js_1.part.js.map
