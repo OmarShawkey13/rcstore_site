02e1db6ee31e74c71150eb26ca8e8ae452ce7ccb
OmarShawkey13 <superbad13245@gmail.com>
2026-06-11 13:33:30 +0300

    fix

--- main.client.dart.js_1.part.js
+++ main.client.dart.js_1.part.js
@@ -6,7 +6,6 @@ _.f=d
 _.w=e
 _.Q=f
 _.a=g},
-<<<<<<< HEAD
 l2(){return new B.az(null)},
 az:function az(d){this.a=d},
 cJ:function cJ(){var _=this
@@ -19,17 +18,6 @@ fP:function fP(d){this.a=d},
 fM:function fM(d){this.a=d},
 fN:function fN(d){this.a=d},
 fO:function fO(d){this.a=d}},A,D,E
-=======
-l3(){return new B.aB(null)},
-aB:function aB(d){this.a=d},
-cJ:function cJ(){var _=this
-_.e=_.d=!1
-_.c=_.a=_.f=null},
-fR:function fR(d){this.a=d},
-fO:function fO(d){this.a=d},
-fP:function fP(d){this.a=d},
-fQ:function fQ(d){this.a=d}},A,D,E
->>>>>>> 9e9578e481f552a1e93e475c7a7ba490d606a980
 C=c[0]
 B=a.updateHolder(c[3],B)
 A=c[6]
@@ -40,21 +28,14 @@ C(d){var x=null
 return new C.J("nav",x,this.d,x,x,x,this.w,x)}}
 B.eu.prototype={
 C(d){var x=y.g,w=C.K(x,y.a)
-<<<<<<< HEAD
 w.I(0,D.jT().$1$1$onClick(this.f,y.f))
 return new C.J("button",null,this.w,null,C.K(x,x),w,this.Q,null)}}
 B.az.prototype={
-=======
-w.I(0,D.jU().$1$1$onClick(this.f,y.f))
-return new C.J("button",null,this.w,null,C.K(x,x),w,this.Q,null)}}
-B.aB.prototype={
->>>>>>> 9e9578e481f552a1e93e475c7a7ba490d606a980
 bg(){return new B.cJ()}}
 B.cJ.prototype={
 aj(){var x,w=this
 w.aY()
 w.bE()
-<<<<<<< HEAD
 x=C.ie(new B.fQ(w))
 w.f=x
 C.u(b.G.window).addEventListener("scroll",x)},
@@ -89,59 +70,23 @@ B.fN.prototype={
 $0(){return this.a.e=!1},
 $S:0}
 B.fO.prototype={
-=======
-x=C.jB(new B.fR(w))
-w.f=x
-C.w(b.G.window).addEventListener("scroll",x)},
-bE(){var x=this,w=C.id(C.w(b.G.window).scrollY)
-if(w>50&&!x.e)x.a7(new B.fO(x))
-else if(w<=50&&x.e)x.a7(new B.fP(x))},
-aI(){var x=this.f
-if(x!=null)C.w(b.G.window).removeEventListener("scroll",x)
-this.bA()},
-cZ(){this.a7(new B.fQ(this))},
-C(d){var x=this,w=null,v="mobile-nav-link",u=x.e?" scrolled":"",t=x.d?" menu-open":"",s=x.gcY(),r=y.p,q=D.a1(C.f([A.b2,A.ba,D.a1(C.f([A.be,new B.eu(s,"menu-toggle",A.ae,w)],r),w,"nav-right")],r),w,"container navbar-inner"),p=x.d?" active":""
-return new B.ex("navbar"+u+t,C.f([q,D.a1(C.f([D.hu(A.t,v,"/",s),D.hu(A.r,v,"/#apps",s),D.hu(A.a8,v,"/about",s),A.bc],r),w,"mobile-menu"+p)],r),w)}}
-var z=a.updateTypes(["~()"])
-B.fR.prototype={
-$1(d){C.w(d)
-this.a.bE()},
-$S:40}
-B.fO.prototype={
-$0(){return this.a.e=!0},
-$S:0}
-B.fP.prototype={
-$0(){return this.a.e=!1},
-$S:0}
-B.fQ.prototype={
->>>>>>> 9e9578e481f552a1e93e475c7a7ba490d606a980
 $0(){var x=this.a
 x.d=!x.d},
 $S:0};(function installTearOffs(){var x=a._instance_0u
 x(B.cJ.prototype,"gcY","cZ",0)})();(function inheritance(){var x=a.inheritMany,w=a.inherit
 x(D.C,[B.ex,B.eu])
-<<<<<<< HEAD
 w(B.az,D.ak)
 w(B.cJ,D.Y)
 x(C.ae,[B.fQ,B.fP])
 x(C.bk,[B.fM,B.fN,B.fO])})()
 C.fZ(b.typeUniverse,JSON.parse('{"ex":{"C":[],"l":[]},"eu":{"C":[],"l":[]},"az":{"ak":[],"l":[]},"cJ":{"Y":["az"],"Y.T":"az"}}'))
 var y={c:C.E("W<b,b>"),p:C.E("v<l>"),g:C.E("b"),f:C.E("~"),a:C.E("~(o)")};(function constants(){var x=a.makeConstList
-=======
-w(B.aB,D.al)
-w(B.cJ,D.Z)
-w(B.fR,C.af)
-x(C.bk,[B.fO,B.fP,B.fQ])})()
-C.h_(b.typeUniverse,JSON.parse('{"ex":{"C":[],"l":[]},"eu":{"C":[],"l":[]},"aB":{"al":[],"l":[]},"cJ":{"Z":["aB"],"Z.T":"aB"}}'))
-var y={c:C.E("W<b,b>"),p:C.E("t<l>"),g:C.E("b"),f:C.E("~"),a:C.E("~(o)")};(function constants(){var x=a.makeConstList
->>>>>>> 9e9578e481f552a1e93e475c7a7ba490d606a980
 A.az=new C.B("Apps",null)
 A.r=x([A.az],y.p)
 A.aB=new C.B("Home",null)
 A.t=x([A.aB],y.p)
 A.aD=new C.B("About Us",null)
 A.a8=x([A.aD],y.p)
-<<<<<<< HEAD
 A.k=new D.a_("hamburger-bar",null,E.i,null)
 A.ae=x([A.k,A.k,A.k],y.p)
 A.aE=new C.B("RCMods",null)
@@ -156,27 +101,10 @@ A.a6=x([A.ay],y.p)
 A.b1=new D.a7("/about",null,"nav-link",null,A.a6,null)
 A.aa=x([A.b3,A.b6,A.b1],y.p)
 A.ba=new D.a_("nav-links",null,A.aa,null)
-=======
-A.k=new D.a0("hamburger-bar",null,E.i,null)
-A.ae=x([A.k,A.k,A.k],y.p)
-A.aE=new C.B("RCMods",null)
-A.a7=x([A.aE],y.p)
-A.bp=new D.aS(null,A.a7,null)
-A.ai=x([E.D,A.bp],y.p)
-A.b2=new D.a8("/",null,"brand",null,A.ai,null)
-A.b3=new D.a8("/",null,"nav-link",null,A.t,null)
-A.b6=new D.a8("/#apps",null,"nav-link",null,A.r,null)
-A.ay=new C.B("About",null)
-A.a6=x([A.ay],y.p)
-A.b1=new D.a8("/about",null,"nav-link",null,A.a6,null)
-A.aa=x([A.b3,A.b6,A.b1],y.p)
-A.ba=new D.a0("nav-links",null,A.aa,null)
->>>>>>> 9e9578e481f552a1e93e475c7a7ba490d606a980
 A.at={target:0,rel:1}
 A.v=new C.W(A.at,["_blank","noopener noreferrer"],y.c)
 A.bj=new D.bG("Telegram","/images/telegram.webp","social-icon",null)
 A.q=x([A.bj],y.p)
-<<<<<<< HEAD
 A.b4=new D.a7("https://t.me/RCModsWA",null,null,A.v,A.q,null)
 A.bi=new D.bG("Instagram","/images/instagram.webp","social-icon",null)
 A.u=x([A.bi],y.p)
@@ -191,20 +119,4 @@ A.b_=new D.a7("https://instagram.com/x_x_shawkey_x_x",null,null,A.ap,A.u,null)
 A.ad=x([A.b0,A.b_],y.p)
 A.be=new D.a_("nav-socials",null,A.ad,null)})()};
 (a=>{a["53ks6IT2LfLR42Rro0GB87GUB0Y="]=a.current})($__dart_deferred_initializers__);
-=======
-A.b4=new D.a8("https://t.me/RCModsWA",null,null,A.v,A.q,null)
-A.bi=new D.bG("Instagram","/images/instagram.webp","social-icon",null)
-A.u=x([A.bi],y.p)
-A.b5=new D.a8("https://instagram.com/x_x_shawkey_x_x",null,null,A.v,A.u,null)
-A.ag=x([A.b4,A.b5],y.p)
-A.bc=new D.a0("mobile-menu-socials",null,A.ag,null)
-A.x={target:0,rel:1,title:2}
-A.ao=new C.W(A.x,["_blank","noopener noreferrer","Telegram"],y.c)
-A.b0=new D.a8("https://t.me/RCModsWA",null,null,A.ao,A.q,null)
-A.ap=new C.W(A.x,["_blank","noopener noreferrer","Instagram"],y.c)
-A.b_=new D.a8("https://instagram.com/x_x_shawkey_x_x",null,null,A.ap,A.u,null)
-A.ad=x([A.b0,A.b_],y.p)
-A.be=new D.a0("nav-socials",null,A.ad,null)})()};
-(a=>{a["Ve/GSDsFVi1u2n7uao/KvD7rr8Y="]=a.current})($__dart_deferred_initializers__);
->>>>>>> 9e9578e481f552a1e93e475c7a7ba490d606a980
 //# sourceMappingURL=main.client.dart.js_1.part.js.map
