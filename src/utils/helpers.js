3c8ecd7c2b255b2aa02e8b7567def8a1f7f9f6e3
OmarShawkey13 <superbad13245@gmail.com>
2026-07-27 00:54:08 +0300

    change

--- src/utils/helpers.js
+++
@@ -1,8 +0,0 @@
-export function escapeHtml(str) {
-    if (!str) return '';
-    return str.toString().replace(/&/g, '&amp;')
-              .replace(/</g, '&lt;')
-              .replace(/>/g, '&gt;')
-              .replace(/"/g, '&quot;')
-              .replace(/'/g, '&#039;');
-}
